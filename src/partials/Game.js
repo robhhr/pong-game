541636dbfde25b885f65f8e20484199e7557c787
Roberto <robhhr@outlook.com>
2019-05-07 20:42:01 -0700

    SVG container created

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,8 +1,16 @@
+import { SVG_NS } from '../settings';
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
+    let svg = document.createElementNS(SVG_NS, "svg");
+    this.gameElement = document.getElementById(this.element);
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
 
 		// Other code goes here...
   }
